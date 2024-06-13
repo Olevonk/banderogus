c40e36ede8d8a8c5fe825c6e1f7f2a855f2d0fbc
Olevonk <olevonk@gmail.com>
2024-06-13 14:56:08 -0500

    fix bugs

--- js/form.js
+++ js/form.js
@@ -4,8 +4,6 @@ const goToFormButton = document.querySelector('#go-to-form-btn');
 const userEmailField = document.querySelector('#user-email');
 const userNameField = document.querySelector('#user-name');
 
-
-
 goToFormButton.addEventListener('click', function (e) {
     e.preventDefault();
     form.scrollIntoView();
@@ -32,31 +30,33 @@ function showGooseAnim() {
 
     gooseEl.setAttribute('src', './img/gus-anim.gif');
     
-
     setTimeout(() => {
         gooseEl.removeAttribute('src');
-    }, 4000)
+    }, 4000);
 }
 
 addGooseElement();
 
-form.addEventListener('submit', e => {
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(form);
 
-    if (userEmailField?.value?.length > 30) {
+    if (userEmailField?.value?.length > 60) {
         return;
     }
 
-    console.log('Імʼя користувача: ', userEmailField.value);
-    console.log('Email користувача: ', userNameField.value);
+    console.log('Імʼя користувача: ', userNameField.value);
+    console.log('Email користувача: ', userEmailField.value);
 
     launchBtn.setAttribute('disabled', true);
     launchBtn.style.opacity = '0.7';
 
     showGooseAnim();
 
+    launchBtn.removeAttribute("disabled");
+    clearFormFields();
+
     setTimeout(() => {
         launchBtn.style.opacity = '1';
-    }, 4000)
-})
+    }, 4000);
+});
